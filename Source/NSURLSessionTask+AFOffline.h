9dc06f22123e299028409355f3b106c8410f94b4
Elvis Nuñez <elvisnunez@me.com>
2014-11-20 04:10:54 +0100

    Add coding compliant properties

--- Source/NSURLSessionTask+AFOffline.h
+++ Source/NSURLSessionTask+AFOffline.h
@@ -1,7 +1,14 @@
 @import Foundation;
 
-@interface NSURLSessionTask (AFOffline)
+@interface NSURLSessionTask (AFOffline) <NSCoding>
+
+@property (nonatomic, strong) NSURLRequest *af_originalRequest;
+@property (nonatomic, strong) NSURLRequest *af_currentRequest;
+@property (nonatomic, strong) NSURLResponse *af_response;
+@property (nonatomic, strong) NSDictionary *af_params;
 
 - (void)saveWithParams:(NSDictionary *)params;
+
++ (NSArray *)offlineTasks;
 
 @end
